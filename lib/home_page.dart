abc3339e2e83174404c0feb84b54dd8dc7564d21
binodcoder <binodcoder@gmail.com>
2023-04-13 17:44:35 +0545

    file managed

---
+++ lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const batteryChannel = MethodChannel('com.example.native_android_api_call');
+  String batteryLevel = 'Waiting ...';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          batteryLevel,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getBatteryLevel,
+        tooltip: 'check battery',
+        child: const Icon(Icons.battery_0_bar),
+      ),
+    );
+  }
+
+  Future getBatteryLevel() async {
+    final arguments = {'name': 'Sarah Abs'};
+    final int newBatteryLevel = await batteryChannel.invokeMethod('getBatteryLevel', arguments);
+
+    setState(() {
+      batteryLevel = '$newBatteryLevel';
+    });
+  }
+}
